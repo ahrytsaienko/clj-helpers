210d8d080e8ef8a8ecbc05337c1ef67c7044f955
a.hrytsaienko <a.hrytsaienko@modnakasta.ua>
2018-03-11 13:50:58 +0200

    Destructuring

---
+++ src/clj_helpers/.#destructuring.clj
@@ -0,0 +1,1 @@
+artemhrytsaienko@temsan.15457
--- src/clj_helpers/conditions.clj
+++ src/clj_helpers/conditions.clj
@@ -11,9 +11,10 @@
 (if-not false 1 0)
 
 ;Если условие выполнится, запишет в перменную последн. выражение (5)
-(if-let [x (< 7 3)] 
-        5 
+(if-let [x (< 7 3)]
+        5
         7)
+
 
 ;return value if only when-not
 (when-not (< 15 10) (println "test when not") 55)
@@ -23,19 +24,30 @@
 (when-let [a (> 1 5)] "ok")
 
 ;conditions
-(cond  
-    false 1 
+(cond
+    false 1
     false 2
     :else "default")
 
 ;Первое значение то что будет тестироваться
-(case "oki" 
-    1 1 
-    2 2 
+(case "oki"
+    1 1
+    2 2
     "ok" "ok"
     "default")
 
 
-(defn testing [value]
-  (cond (contains? value price) {:a1 :b2}))
-  
+(defn teting [question answer]
+  (str "Your question is " question "and your answer is " answer))
+
+(teting "What is your name" "Your answwwe")
+
+(defn testing-function [a b]
+  (+ a b))
+
+
+
+
+
+
+
